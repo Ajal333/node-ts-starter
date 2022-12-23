6576534bf82652bfa27b3fc924f1b55782ab8380
ajal333 <ajapoos1999@gmail.com>
2022-12-23 16:56:05 +0530

    feat: project init with typescript

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port: number = 3000;
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({ youGot: "Lucky!" });
+});
+
+app.listen(port, () => {
+  try {
+    return console.log(`Server is listening on ${port}`);
+  } catch (error) {
+    return console.error(error);
+  }
+});
